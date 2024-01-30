7c886023bd5fb8f3820e426d9c579360d24731c1
subhashraaj <150801940+subhashraaj@users.noreply.github.com>
2024-01-30 12:17:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-//your JS code here. If required.
+function oddNumber(){
+    const arr = [1, 2, 3, 4, 5];
+    let oddArr= [];
+    //console.log(arr)
+    return new Promise((resolve, reject)=>{
+        for(let i=0; i<arr.length; i++){
+            if(arr[i] % 2 != 0){
+                oddArr.push(arr[i])
+            }
+        }
+        setTimeout(()=>{
+            //console.log(new Date)
+            document.querySelector("#output").innerText=oddArr;
+            resolve(arr)
+        }, 1000)
+    });
+}
+function multiplyEven(arr){
+    let evenArr= [];
+    return new Promise((resolve, reject)=>{
+        for(let i=0; i<arr.length; i++){
+            if(arr[i] % 2 === 0){
+                evenArr.push(arr[i]);
+            }
+        }
+        setTimeout(()=>{
+            //console.log(new Date)
+            document.querySelector("#output").innerText=evenArr;
+            resolve(evenArr)
+        }, 2000)
+
+    })
+}
+
+oddNumber()
+    .then((arr)=>{
+        return multiplyEven(arr)
+    })
+    .then((evenArr)=>{
+        //console.log(new Date)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+
